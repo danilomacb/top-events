dfb2bcdf46410306ca12335766e9a016bb1d81da
danilomacb <danilo.bakun@gmail.com>
2021-03-01 07:45:16 -0300

    using config variables

--- index.js
+++ index.js
@@ -5,41 +5,46 @@ const schedule = require("node-schedule");
 
 const client = new Discord.Client();
 
+const channelId = "815892116135739443";
+const messageDelay = 5;
+const fcMessage = `FC in ${messageDelay} minutes!`;
+const minutes = 60 - messageDelay;
+
 client.on("ready", async () => {
   console.log("Bot running");
 
-  channel = await client.channels.fetch("800792617091727380");
+  channel = await client.channels.fetch(channelId);
 
-  schedule.scheduleJob("55 1 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 1 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 4 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 4 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 7 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 7 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 10 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 10 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 13 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 13 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 16 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 16 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 19 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 19 * * *`, () => {
+    channel.send(fcMessage);
   });
 
-  schedule.scheduleJob("55 22 * * *", () => {
-    channel.send("@everyone FC in 5 minutes!");
+  schedule.scheduleJob(`${minutes} 22 * * *`, () => {
+    channel.send(fcMessage);
   });
 });
 
