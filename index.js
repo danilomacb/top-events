c5fa7bf5fce78c692ad49e2806952cfc70781b38
danilomacb <danilo.bakun@gmail.com>
2021-03-01 18:18:37 -0300

    scheduling more events and making a better code

--- index.js
+++ index.js
@@ -6,46 +6,45 @@ const schedule = require("node-schedule");
 const client = new Discord.Client();
 
 const channelId = "815892116135739443";
-const messageDelay = 5;
-const fcMessage = `FC in ${messageDelay} minutes!`;
+
+const messageDelay = 10;
 const minutes = 60 - messageDelay;
+
+const role = "<@&816040318096375868>";
+
+function scheduleMessage(hour, eventName) {
+  schedule.scheduleJob(`${minutes} ${hour--} * * *`, () => {
+    channel.send(`${role} ${eventName} in ${messageDelay} minutes!`);
+  });
+}
 
 client.on("ready", async () => {
   console.log("Bot running");
 
-  channel = await client.channels.fetch(channelId);
+  try {
+    channel = await client.channels.fetch(channelId);
+  } catch (err) {
+    console.log("Fail to get discord channel", err);
+    return;
+  }
 
-  schedule.scheduleJob(`${minutes} 0 * * *`, () => {
-    channel.send(fcMessage);
-  });
+  scheduleMessage(1, "FC and DS");
+  scheduleMessage(4, "FC and DS");
+  scheduleMessage(7, "FC and DS");
+  scheduleMessage(10, "FC and DS");
+  scheduleMessage(13, "FC and DS");
+  scheduleMessage(16, "FC and DS");
+  scheduleMessage(19, "FC and DS");
+  scheduleMessage(22, "FC and DS");
 
-  schedule.scheduleJob(`${minutes} 3 * * *`, () => {
-    channel.send(fcMessage);
-  });
+  scheduleMessage(11, "CD");
+  scheduleMessage(23, "CD");
 
-  schedule.scheduleJob(`${minutes} 6 * * *`, () => {
-    channel.send(fcMessage);
-  });
+  scheduleMessage(3, "CI");
+  scheduleMessage(15, "CI");
 
-  schedule.scheduleJob(`${minutes} 9 * * *`, () => {
-    channel.send(fcMessage);
-  });
-
-  schedule.scheduleJob(`${minutes} 12 * * *`, () => {
-    channel.send(fcMessage);
-  });
-
-  schedule.scheduleJob(`${minutes} 15 * * *`, () => {
-    channel.send(fcMessage);
-  });
-
-  schedule.scheduleJob(`${minutes} 18 * * *`, () => {
-    channel.send(fcMessage);
-  });
-
-  schedule.scheduleJob(`${minutes} 21 * * *`, () => {
-    channel.send(fcMessage);
-  });
+  scheduleMessage(6, "CTF");
+  scheduleMessage(21, "CTF");
 });
 
 client.login(process.env.TOKEN);
