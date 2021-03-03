56a51b2d9f58f878efc7509bcb018d3b26a6533f
danilomacb <danilo.bakun@gmail.com>
2021-03-03 17:36:48 -0300

    ca, dw and sw

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const minutes = 60 - messageDelay;
 
 const role = "<@&816040318096375868>";
 
-function scheduleMessage(hour, eventName) {
-  schedule.scheduleJob(`${minutes} ${--hour} * * *`, async () => {
+function scheduleMessage(hour, dayOfWeek, eventName) {
+  schedule.scheduleJob(`${minutes} ${--hour} * * ${dayOfWeek}`, async () => {
     let sendedMessage;
     try {
       sendedMessage = await channel.send(`${role} ${eventName} in ${messageDelay} minutes!`);
@@ -38,23 +38,38 @@ client.on("ready", async () => {
     return;
   }
 
-  scheduleMessage(1, "FC and DS");
-  scheduleMessage(4, "FC and DS");
-  scheduleMessage(7, "FC and DS");
-  scheduleMessage(10, "FC and DS");
-  scheduleMessage(13, "FC and DS");
-  scheduleMessage(16, "FC and DS");
-  scheduleMessage(19, "FC and DS");
-  scheduleMessage(22, "FC and DS");
+  scheduleMessage(1, "*", "FC and DS");
+  scheduleMessage(4, "*", "FC and DS");
+  scheduleMessage(7, "*", "FC and DS");
+  scheduleMessage(10, "*", "FC and DS");
+  scheduleMessage(13, "*", "FC and DS");
+  scheduleMessage(16, "*", "FC and DS");
+  scheduleMessage(19, "*", "FC and DS");
+  scheduleMessage(22, "*", "FC and DS");
 
-  scheduleMessage(11, "CD");
-  scheduleMessage(23, "CD");
+  scheduleMessage(11, "*", "CD");
+  scheduleMessage(23, "*", "CD");
 
-  scheduleMessage(3, "CI");
-  scheduleMessage(15, "CI");
+  scheduleMessage(3, "*", "CI");
+  scheduleMessage(15, "*", "CI");
 
-  scheduleMessage(6, "CTF");
-  scheduleMessage(21, "CTF");
+  scheduleMessage(6, "*", "CTF");
+  scheduleMessage(21, "*", "CTF");
+
+  scheduleMessage(0, "*", "CA");
+  scheduleMessage(9, "*", "CA");
+  scheduleMessage(17, "*", "CA");
+
+  scheduleMessage(3, "*", "DW");
+  scheduleMessage(7, "*", "DW");
+  scheduleMessage(11, "*", "DW");
+  scheduleMessage(15, "*", "DW");
+  scheduleMessage(19, "*", "DW");
+  scheduleMessage(23, "*", "DW");
+
+  scheduleMessage(13, 2, "SW");
+  scheduleMessage(13, 4, "SW");
+  scheduleMessage(13, 6, "SW");
 });
 
 client.login(process.env.TOKEN);
